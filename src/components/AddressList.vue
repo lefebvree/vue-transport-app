44c8044c56d0a4c88e7b0297638a818a58810d6b
Etienne Lefebvre <etiennelefebvre@outlook.com>
2018-02-13 16:08:30 +0100

    Add AddressList Component

---
+++ src/components/AddressList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class='' v-for="address in addresses" :key="address.name">
+      <div class='content'>
+        <div class='header'>
+          {{ address }}
+        </div>
+        <div class='meta'>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+export default {
+  props: ['addresses'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/components/MyAddresses.vue
+++ src/components/MyAddresses.vue
@@ -1,19 +1,39 @@
 <template>
-  <div>
-    Mes adresses
+  <div id="myaddresses">
+    <h1>Mes adresses</h1>
+    <address-list :addresses="addresses"></address-list>
   </div>
 </template>
 
 <script>
+import AddressList from '@/components/AddressList'
+
 export default {
   name: 'MyAddresses',
+  components: {
+    // Reference to the AddressList component
+    AddressList
+  },
   data () {
     return {
+      addresses: [{
+        name: 'Maison',
+        address: '34 rue Bolivar, 69005, Lyon',
+        favorite: true
+      }, {
+        name: 'Travail',
+        address: '43, bd du 11 novembre 1918, 69622 Villeurbanne cedex',
+        favorite: true
+      }, {
+        name: 'Nautibus',
+        address: '43, bd du 11 novembre 1918, 69622 Villeurbanne cedex',
+        favorite: false
+      }, {
+        name: 'AML',
+        address: '43, bd du 11 novembre 1918, 69622 Villeurbanne cedex',
+        favorite: false
+      }]
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
